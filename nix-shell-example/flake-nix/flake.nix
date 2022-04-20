32e1da40f087235d2295ac7aad24b31a0df2987b
Robert Schardt <robert.schardt@iohk.io>
2022-04-20 12:59:57 +0200

    Add basic flake.

--- nix-shell-example/flake-nix/flake.nix
+++ nix-shell-example/flake-nix/flake.nix
@@ -1,7 +1,29 @@
 {
   description = "Example flake";
 
-  inputs = {};
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
 
-  outputs = {};
+  outputs = { self, nixpkgs }:
+  let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  in
+  {
+    # Note that 'nix shell' only works
+    # when defaultPackage or another output is a pkgs.buildEnv or mkShell
+    defaultPackage.x86_64-linux = pkgs.stdenv.mkDerivation {
+
+      name = "exampleApp";
+      src = ./.;
+      buildInputs = with pkgs; [
+        lolcat
+      ];
+      installPhase = ''
+        mkdir -p $out
+        echo "hello world" | tee example.txt
+        mv example.txt $out
+      '';
+    };
+  };
 }
