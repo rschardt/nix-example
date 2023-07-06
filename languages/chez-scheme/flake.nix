66f6fa4e0da3b7649c8728fe66b6c08129e517fd
Robert Schardt <robert.schardt@iohk.io>
2023-07-06 17:50:03 +0200

    Add nix-bundle and document more strategies

--- languages/chez-scheme/flake.nix
+++ languages/chez-scheme/flake.nix
@@ -3,15 +3,18 @@
 
   inputs = {
     utils.url = "github:numtide/flake-utils";
+    nix-bundle.url = "github:matthewbauer/nix-bundle";
   };
 
   outputs = {
     self,
     nixpkgs,
     utils,
+    nix-bundle,
     ...
   }:
-    utils.lib.eachDefaultSystem (
+    #utils.lib.eachDefaultSystem (
+    utils.lib.eachSystem ["x86_64-linux"] (
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
@@ -44,6 +47,22 @@
                 mv hello-world $out/bin/hello-world
               '';
             };
+
+            # 0. not working with 'nix bundle'
+            # 1.(nix-bundle mathewbauer) not working for aarch64-darwin
+            # 1a. only working for [ "x86_64-linux" "i686-linux" "aarch64-linux" ] from https://github.com/matthewbauer/nix-bundle/blob/master/flake.nix
+            # 2. doesn't work with flake pkgs, maybe works with pkgs from nixpkgs
+            # try overriding nixpkgs:  https://nixos.org/guides/nix-pills/nixpkgs-overriding-packages.html#idm140737319619744
+            # 3A. try to create a self contained binary with chez-exe
+            # 3B. try to modify 'env scheme' at the top of the file and package scheme-interpreter with binary
+            # 4. try to create a platform specific image with boot file
+            binary_withNixBundle = let
+              nixpkgs = nixpkgs.legacyPackages.${system}.override {
+                binary_onlyRunnableOnNixos = packages.binary_onlyRunnableOnNixOS;
+              };
+            in
+              nix-bundle.bundlers.nix-bundle { program=nixpkgs.binary_onlyRunnableOnNixos; system=system; };
+            #binary_withNixBundle = nix-bundle.bundlers.nix-bundle { program=packages.default; system=system; };
           };
 
           apps.default = { type = "app"; program = "${packages.default}/bin/hello-world"; };
