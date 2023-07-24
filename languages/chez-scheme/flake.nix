532719fa496479c3244ff8819f5a6f1e7f091011
Robert Schardt <robert.schardt@iohk.io>
2023-07-24 14:17:00 +0200

    Add instructions/flake-outputs to compile standalone binary

--- languages/chez-scheme/flake.nix
+++ languages/chez-scheme/flake.nix
@@ -3,26 +3,30 @@
 
   inputs = {
     utils.url = "github:numtide/flake-utils";
-    nix-bundle.url = "github:matthewbauer/nix-bundle";
+    chez-exe.url = "github:rschardt/chez-exe";
   };
 
   outputs = {
     self,
     nixpkgs,
     utils,
-    nix-bundle,
     ...
-  }:
-    #utils.lib.eachDefaultSystem (
-    utils.lib.eachSystem ["x86_64-linux"] (
+    } @ inputs:
+    utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
         chez = pkgs.chez-racket;
+        chez-exe = inputs.chez-exe.packages.${system}.default;
       in
         rec {
           packages = rec {
+
             default = binary_onlyRunnableOnNixOS;
+
+            # a chez-scheme binary compiled this way relies
+            # on a scheme-interpreter installed locally
+            # note: this binary will only run on nixos
             binary_onlyRunnableOnNixOS = pkgs.stdenv.mkDerivation {
               name = "hello-world";
               version = "0.0.1";
@@ -48,21 +52,48 @@
               '';
             };
 
-            # 0. not working with 'nix bundle'
-            # 1.(nix-bundle mathewbauer) not working for aarch64-darwin
-            # 1a. only working for [ "x86_64-linux" "i686-linux" "aarch64-linux" ] from https://github.com/matthewbauer/nix-bundle/blob/master/flake.nix
-            # 2. doesn't work with flake pkgs, maybe works with pkgs from nixpkgs
-            # try overriding nixpkgs:  https://nixos.org/guides/nix-pills/nixpkgs-overriding-packages.html#idm140737319619744
-            # 3A. try to create a self contained binary with chez-exe
-            # 3B. try to modify 'env scheme' at the top of the file and package scheme-interpreter with binary
-            # 4. try to create a platform specific image with boot file
-            binary_withNixBundle = let
-              nixpkgs = nixpkgs.legacyPackages.${system}.override {
-                binary_onlyRunnableOnNixos = packages.binary_onlyRunnableOnNixOS;
-              };
-            in
-              nix-bundle.bundlers.nix-bundle { program=nixpkgs.binary_onlyRunnableOnNixos; system=system; };
-            #binary_withNixBundle = nix-bundle.bundlers.nix-bundle { program=packages.default; system=system; };
+            # for a truely self contained binary using
+            # chez-exe(https://github.com/gwatt/chez-exe/tree/master) is advised
+            # note: this binary will only run on nixos
+            binary_standAloneNixos = pkgs.stdenv.mkDerivation {
+              name = "hello-standalone-nixos";
+              version = "0.0.1";
+              src = ./src/.;
+              buildInputs = [
+                chez
+                chez-exe
+                pkgs.libuuid
+              ];
+
+              buildPhase = ''
+                compile-chez-program --optimize-level 2 hello-world.ss
+              '';
+
+              installPhase = ''
+                mkdir -p $out/bin
+                chmod +x hello-world
+                mv hello-world $out/bin/hello-world
+              '';
+            };
+
+            # this binary will run on other linuxes(not nixos)
+            # but only for the x86-64 architecture atm
+            # tested on Archlinux and Ubuntu docker images
+            # note: this binary will not run on nixos but others
+            binary_otherLinuxes = pkgs.stdenv.mkDerivation {
+              name = "abcirdc-binary-otherLinux";
+              version = "0.0.1";
+              src = packages.binary_standAloneNixos;
+
+              installPhase = ''
+                mkdir -p $out/bin
+                cp bin/hello-world $out/bin
+                patchelf --set-rpath /lib64/ld-linux-x86-64.so.2 $out/bin/hello-world
+                patchelf --set-interpreter /lib64/ld-linux-x86-64.so.2 $out/bin/hello-world
+              '';
+
+              dontFixup = true;
+            };
           };
 
           apps.default = { type = "app"; program = "${packages.default}/bin/hello-world"; };
